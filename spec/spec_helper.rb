3da4ad70ca00ca28292fb1b8ba9abb963d94bbd4
Idalis Paz <idalispaz@gmail.com>
2021-01-24 20:54:05 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,13 @@ RSpec.configure do |config|
   config.include Rack::Test::Methods
   config.order = 'default'
 end
+ 
+class MyServer
+  def call(env)
+    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+  end
+ 
+  def pretty_response
+    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+  end
+end
